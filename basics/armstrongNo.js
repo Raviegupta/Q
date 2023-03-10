fd2c2324b09d72389ad8bcb220489c36b57250b4
Raviegupta <raviegupta153@gmail.com>
2023-03-10 09:10:56 +0530

    Armstrong No

---
+++ basics/armstrongNo.js
@@ -0,0 +1,15 @@
+// Armstrong No
+// ex:- raviegupta153@gmail.com, 1634
+
+const armstrongNo = (n) => {
+  n = String(n);
+  let sum = 0;
+  for (let i = 0; i < n.length; i++) {
+    sum += Number(n[i]) ** Number(n.length);
+  }
+  let result = sum == n ? "armstrong" : "no";
+  return result;
+};
+console.log(armstrongNo(153));
+console.log(armstrongNo(1634));
+console.log(armstrongNo(121));
