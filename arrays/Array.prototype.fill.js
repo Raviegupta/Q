42780123dae52a120930e88222313ce87a72d584
Raviegupta <raviegupta153@gmail.com>
2023-03-11 22:54:10 +0530

    Array.prototype.fill() implementation

--- arrays/Array.prototype.fill.js
+++ arrays/Array.prototype.fill.js
@@ -12,3 +12,18 @@ console.log(array);
 
 array.fill(9);
 console.log(array);
+
+
+// ---------------------------------------------------------------------------------------------------------
+// implement Array.prototype.fill()
+const fill = (arr, value, start, end) => {
+    for(let i = start; i < end; i++) {
+        arr[i] = value;
+    }
+    return arr
+}
+console.log(fill([1, 2, 3, 4, 5, 6], 0, 2, 4))
+
+
+// ---------------------------------------------------------------------------------------------------------
+// https://replit.com/@ac102-batch/Change-to-Zero-1-Raviegupta#index.js
