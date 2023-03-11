de3c7df70779b36447bddb152ef0a10b55261f20
Raviegupta <raviegupta153@gmail.com>
2023-03-11 15:29:15 +0530

    Array.isArray()

--- arrays/isArray.js
+++ arrays/isArray.js
@@ -1,18 +1,19 @@
 // Check it is array or not ?
 // https://replit.com/@ac102-batch/Element-Index-1-Raviegupta
 
-const isArray = (arr) => {
-  if (typeof arr == "object") {
-    return "YES";
-  } else {
-    return "NO";
-  }
-};
+// const isArray = (arr) => {
+//   if (typeof arr == "object") {
+//     return "YES";
+//   } else {
+//     return "NO";
+//   }
+// };
 
 // console.log(isArray([2,4,5]));
 // console.log(isArray(7));
+// console.log(isArray({a: 5, b: "Hello World"}));
 
-// -------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------
 // If it is array return the index of given element if it is present, not present return -1. if it is not array return "Not an Array"
 
 const elementIndex = (arr, element) => {
@@ -23,8 +24,22 @@ const elementIndex = (arr, element) => {
   }
 }
 
-console.log(elementIndex([2,4,5], 4));
-console.log(elementIndex([2,4,5], 7));
-console.log(elementIndex(7));
-console.log(elementIndex(['ant', 'bison', 'camel', 'duck', 'bison'], 'duck'));
-console.log(elementIndex(['ant', 'bison', 'camel', 'duck', 'bison'], 'girrafe'));
+// console.log(elementIndex([2,4,5], 4));
+// console.log(elementIndex([2,4,5], 7));
+// console.log(elementIndex(7));
+// console.log(elementIndex(['ant', 'bison', 'camel', 'duck', 'bison'], 'duck'));
+// console.log(elementIndex(['ant', 'bison', 'camel', 'duck', 'bison'], 'girrafe'));
+
+// ---------------------------------------------------------------------------------------------------------
+// Check it is array or object 
+// The isArray() method returns true if an object is an array, otherwise false.
+
+const isArray = (arr) => {
+  let ans = Array.isArray(arr);
+  return ans;
+}
+console.log(isArray([2,4,5]));
+console.log(isArray(7));
+console.log(isArray({a: 5, b: "Hello World"}));
+
+
