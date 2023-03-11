88f76af53ef792bf2d5f388131219d81bb685da0
Raviegupta <raviegupta153@gmail.com>
2023-03-11 23:56:48 +0530

    implement

--- arrays/arrayToString.js
+++ arrays/arrayToString.js
@@ -6,11 +6,23 @@ const arrString = (arr) => {
     let ans =  arr.toString();
     return ans;
 }
-console.log(arrString([1, 5, 3, "ravie"]));
+// console.log(arrString([1, 5, 3, "ravie"]));
 
 
 // method2 :- String()
 const arrString2 = (arr) => {
     return String(arr);
 }
-console.log(arrString2([1, 5, 3, "ravie"]));
+// console.log(arrString2([1, 5, 3, "ravie"]));
+
+
+// implementation
+const arrToString = (arr) => {
+    let str = "";
+    for(let i = 0; i < arr.length-1; i++) {
+       str += arr[i] + ','
+    }
+    str += arr[arr.length-1]
+    return str;
+}
+console.log(arrToString([1, 5, 3, "ravie"]));
