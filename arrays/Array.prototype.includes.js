b7427c442f687be90f48e3607f4d074c7e7425b2
Raviegupta <raviegupta153@gmail.com>
2023-03-11 21:38:12 +0530

    Array.prototype.includes()

---
+++ arrays/Array.prototype.includes.js
@@ -0,0 +1,10 @@
+// Array.prototype.includes() :-
+// returns boolean
+
+const array = [1, 5, 2, 9, 17];
+console.log(array.includes(5));
+console.log(array.includes(7));
+
+const orders = ["paneer pizza", "cake", "cold drink", "chips"];
+let ans = orders.includes("sahee paneer");
+console.log(ans);
