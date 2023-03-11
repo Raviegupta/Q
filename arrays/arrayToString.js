759ac0b36d063ddc6180b2f9ac7789e422d20d14
Raviegupta <raviegupta153@gmail.com>
2023-03-12 00:22:07 +0530

    nested arrar to string

--- arrays/arrayToString.js
+++ arrays/arrayToString.js
@@ -1,21 +1,24 @@
 // convert the given array to string.
 // https://replit.com/@ac102-batch/Array-to-String-2-Raviegupta
 
-// method1 :- Object.prototype.toString() :- returns a string representing the object, By default toString() takes no parameters
+// method1 :- Object.prototype.toString() :-
+// returns a string representing the object, By default toString() takes no parameters.
+// also convert the nested array into string using toString().
 const arrString = (arr) => {
     let ans =  arr.toString();
     return ans;
 }
-// console.log(arrString([1, 5, 3, "ravie"]));
+console.log(arrString([1, 5, 3, "ravie"]));
+console.log(arrString([1, 5, 3, "ravie", ["Hello", "Hi"]]));
 
-
+// ---------------------------------------------------------------------------------------------------------
 // method2 :- String()
 const arrString2 = (arr) => {
     return String(arr);
 }
 // console.log(arrString2([1, 5, 3, "ravie"]));
 
-
+// ---------------------------------------------------------------------------------------------------------
 // implementation
 const arrToString = (arr) => {
     let str = "";
@@ -25,4 +28,9 @@ const arrToString = (arr) => {
     str += arr[arr.length-1]
     return str;
 }
-console.log(arrToString([1, 5, 3, "ravie"]));
+// console.log(arrToString([1, 5, 3, "ravie"]));
+
+
+
+// ---------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------
