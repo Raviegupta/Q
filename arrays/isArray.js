3806aabb7f15c43f47a48456feea229eb4ab6f54
Raviegupta <raviegupta153@gmail.com>
2023-03-11 16:13:20 +0530

    lastIndexOf()

--- arrays/isArray.js
+++ arrays/isArray.js
@@ -13,14 +13,16 @@
 // console.log(isArray(7));
 // console.log(isArray({a: 5, b: "Hello World"}));
 
+
+
 // ----------------------------------------------------------------------------------------------------------
-// If it is array return the index of given element if it is present, not present return -1. if it is not array return "Not an Array"
+// If it is array return the index of given element if it is present, not present return -1. if it is not array return "Not Array"
 
 const elementIndex = (arr, element) => {
   if(typeof(arr) == 'object') {
     return arr.indexOf(element)
   } else {
-    return "Not an Array"
+    return "Not Array"
   }
 }
 
@@ -29,6 +31,34 @@ const elementIndex = (arr, element) => {
 // console.log(elementIndex(7));
 // console.log(elementIndex(['ant', 'bison', 'camel', 'duck', 'bison'], 'duck'));
 // console.log(elementIndex(['ant', 'bison', 'camel', 'duck', 'bison'], 'girrafe'));
+
+
+
+// ---------------------------------------------------------------------------------------------------------
+// If element exist in the array return the index of the element else return "Not Found". If there are multiple occurence of the element return the last occurence of the element
+// https://replit.com/@ac102-batch/Index-Of-1-Raviegupta
+
+const indexOf = (arr, element) => {
+  if(typeof(arr) == 'object') {
+    let ans =  arr.lastIndexOf(element);
+    return ans == -1 ? "Not Found" : ans
+  } else {
+    return "Not Found"
+  }
+}
+
+// Implementation of lastIndexOf()
+function indexOf(arr, element) {
+  if ((typeof (arr) == 'object')) {
+    for (var i = arr.length; i >= 0; i--) {
+      if (arr[i] == element) {
+        return i
+      }
+    } if (i == -1) {
+      return 'Not Found'
+    }
+  } else return 'Not Array'
+}
 
 // ---------------------------------------------------------------------------------------------------------
 // Check it is array or object 
@@ -42,4 +72,7 @@ console.log(isArray([2,4,5]));
 console.log(isArray(7));
 console.log(isArray({a: 5, b: "Hello World"}));
 
+
+
+// ---------------------------------------------------------------------------------------------------------
 
