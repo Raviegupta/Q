860b46926236c496220249aeca395a70a7e21f08
Raviegupta <raviegupta153@gmail.com>
2023-03-12 19:42:45 +0530

    test

---
+++ testing/index.js
@@ -0,0 +1,34 @@
+// console.log('script start'); --------1
+
+// setTimeout(function () {
+// console.log('setTimeout'); --------2
+// }, 0);
+
+// Promise.resolve()
+// .then(function () {
+// console.log('promise1'); --------3
+// })
+// .then(function () {
+// console.log('promise2'); --------4
+// });
+
+// console.log('script end'); --------5
+
+
+const push = (arr, element) => {
+    arr[arr.length] = element;
+}
+console.log(push([1,2,3,4], 5));
+
+
+
+
+
+
+
+
+
+
+
+
+
