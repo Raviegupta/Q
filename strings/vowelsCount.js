a253c9e8538671d4829bcf5d827597744770e88b
Raviegupta <raviegupta153@gmail.com>
2023-03-12 23:31:19 +0530

    vowels count

---
+++ strings/vowelsCount.js
@@ -0,0 +1,23 @@
+// Number of vowels present in the string
+
+const vowels = (str) => {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] == "A" ||
+      str[i] == "E" ||
+      str[i] == "I" ||
+      str[i] == "O" ||
+      str[i] == "U" ||
+      str[i] == "a" ||
+      str[i] == "e" ||
+      str[i] == "i" ||
+      str[i] == "o" ||
+      str[i] == "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+};
+console.log(vowels('beErcAmpuS'));
