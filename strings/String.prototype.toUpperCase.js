d9a803dfd71793fe697c2d37c198f86c5326350c
Raviegupta <raviegupta153@gmail.com>
2023-03-12 23:42:01 +0530

    String.prototype.toUpperCase()

---
+++ strings/String.prototype.toUpperCase.js
@@ -0,0 +1,8 @@
+// String.prototype.toUpperCase()
+// returns a new string representing the calling string converted to upper case.
+// This method does not affect the value of the string itself since JavaScript strings are immutable.
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+let newStr = sentence.toUpperCase();
+console.log(sentence);
+console.log(newStr);
