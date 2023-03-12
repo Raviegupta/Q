7558268495ac2e95da04c0e3c8235e133dbc7abd
Raviegupta <raviegupta153@gmail.com>
2023-03-12 23:55:26 +0530

    beetroot

--- puzzles/puzzle.js
+++ puzzles/puzzle.js
@@ -1,1 +1,5 @@
-// questions
+
+// beetroot
+// https://www.geeksforgeeks.org/puzzle-the-circle-of-lights/
+// https://www.geeksforgeeks.org/puzzle-round-table-coin-game/
+// https://www.geeksforgeeks.org/puzzle-6-monty-hall-problem/
