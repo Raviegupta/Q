7cc9b86c5237b830559f24e11f0e53ed474b7bfd
Raviegupta <raviegupta153@gmail.com>
2023-03-12 23:59:33 +0530

    ahmedabad

--- puzzles/puzzle.js
+++ puzzles/puzzle.js
@@ -3,3 +3,8 @@
 // https://www.geeksforgeeks.org/puzzle-the-circle-of-lights/
 // https://www.geeksforgeeks.org/puzzle-round-table-coin-game/
 // https://www.geeksforgeeks.org/puzzle-6-monty-hall-problem/
+
+// ahmedabad
+// https://www.geeksforgeeks.org/interview-puzzle-the-shopkeeper-and-the-lady-who-made-a-purchase-of-rs-200-with-fake-note/
+// https://www.geeksforgeeks.org/puzzle-12-maximize-probability-of-white-ball/
+// https://www.geeksforgeeks.org/puzzle-heaven-hell/
