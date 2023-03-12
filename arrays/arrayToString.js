2036b0082fcd2d11408358d789454b58aa5d015c
Raviegupta <raviegupta153@gmail.com>
2023-03-12 08:16:05 +0530

    String.prototype.split()

--- arrays/arrayToString.js
+++ arrays/arrayToString.js
@@ -8,8 +8,8 @@ const arrString = (arr) => {
     let ans =  arr.toString();
     return ans;
 }
-console.log(arrString([1, 5, 3, "ravie"]));
-console.log(arrString([1, 5, 3, "ravie", ["Hello", "Hi"]]));
+// console.log(arrString([1, 5, 3, "ravie"]));
+// console.log(arrString([1, 5, 3, "ravie", ["Hello", "Hi"]]));
 
 // ---------------------------------------------------------------------------------------------------------
 // method2 :- String()
@@ -33,4 +33,23 @@ const arrToString = (arr) => {
 
 
 // ---------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------------------------------------------------
+// ---------------------------------------String.prototype.split()------------------------------------------
+
+// convert the string back to the array, we will use the split() method.
+// String.prototype.split() :-
+// The split() method takes a pattern for dividing the string by searching the pattern.
+// divides a String into an ordered list of substrings, puts these substrings into an array, and returns the array.
+// strings are unmutable, so 🤣 enjoy.
+
+const str = 'The quick brown fox, jumps over the lazy dog.';
+let words = str.split(' ');     
+console.log(words);
+console.log(words[3]);
+console.log(str);
+
+let chars = str.split('')
+console.log(chars);
+console.log(chars[5]);
+
+let strCopy = str.split();
+console.log(strCopy);
