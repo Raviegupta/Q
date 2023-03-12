dc70923bc87ede43809ce33bd39f82fc9530b9df
Raviegupta <raviegupta153@gmail.com>
2023-03-12 15:18:02 +0530

    Array.prototype.shift()

--- arrays/Array.prototype.pop.js
+++ arrays/Array.prototype.pop.js
@@ -1,5 +1,7 @@
 // Array.prototype.pop() :-
 // removes the last element from an array and returns that element.
+// mutating method. It changes the length and the content of array.
+// returns undefined if the array is empty.
 
 const plants = ["broccoli", "cauliflower", "cabbage", "kale", "tomato"];
 let removedElement = plants.pop();
@@ -21,8 +23,29 @@ const pop = (arr) => {
   }
   return newArr;
 };
-console.log(pop(["broccoli", "cauliflower", "cabbage", "kale", "tomato"]));
+// console.log(pop(["broccoli", "cauliflower", "cabbage", "kale", "tomato"]));
+
+
+
 
 
 // ---------------------------------------------------------------------------------------------------------
+// -------------------------------------Array.prototype.shift()---------------------------------------------
+// Array.prototype.shift() :- has similar behavior to pop(), but applied to the first element in an array.
+
+const myFish = ["angel", "clown", "mandarin", "surgeon"];
+const shifted = myFish.shift();
+// console.log(shifted);
+// console.log(myFish);
+
+
+// implement
+const shift = (arr) => {
+  let newArr = [];
+  for (let i = 1; i < arr.length; i++) {
+    newArr[i - 1] = arr[i];
+  }
+  return newArr;
+};
+console.log(shift(["angel", "clown", "mandarin", "surgeon"]))
 
