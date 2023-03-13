365b110b26e6c35d6bbc44f06ceb695e1964892e
Raviegupta <raviegupta153@gmail.com>
2023-03-13 23:50:18 +0530

    String.prototype.trimStart()

--- strings/String.prototype.trim.js
+++ strings/String.prototype.trim.js
@@ -7,4 +7,15 @@ console.log(greeting.trim());
 let newStr = greeting.trim();
 console.log(newStr)
 
+
+
+
 // ---------------------------------------------------------------------------------------------------------
+// To return a new string with whitespace trimmed from just one end, use trimStart() or trimEnd().
+// String.prototype.trimStart() :-
+
+const greeting2 = '   Hello world!   ';
+console.log(greeting2.trimStart());
+
+let newStr2 = greeting2.trimStart();
+console.log(newStr2)
