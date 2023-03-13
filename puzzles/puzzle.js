ac2c6bcf6c260bb9ab33de65ea3996ff67ab29aa
Raviegupta <raviegupta153@gmail.com>
2023-03-13 22:48:14 +0530

    doremon

--- puzzles/puzzle.js
+++ puzzles/puzzle.js
@@ -8,3 +8,9 @@
 // https://www.geeksforgeeks.org/interview-puzzle-the-shopkeeper-and-the-lady-who-made-a-purchase-of-rs-200-with-fake-note/
 // https://www.geeksforgeeks.org/puzzle-12-maximize-probability-of-white-ball/
 // https://www.geeksforgeeks.org/puzzle-heaven-hell/
+
+// doremon
+// https://www.geeksforgeeks.org/puzzle-22-maximum-chocolates/
+// https://www.geeksforgeeks.org/puzzle-17-ratio-of-boys-and-girls-in-a-country-where-people-want-only-boys/
+// https://www.geeksforgeeks.org/puzzle-5-finding-the-injection-for-anesthesia/
+// https://www.geeksforgeeks.org/puzzle-20-5-pirates-and-100-gold-coins/
