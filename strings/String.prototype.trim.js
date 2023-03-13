82783811cf161df89f33ca1717586bd7c3d10b2e
Raviegupta <raviegupta153@gmail.com>
2023-03-13 23:47:38 +0530

    String.prototype.trim()

---
+++ strings/String.prototype.trim.js
@@ -0,0 +1,10 @@
+// String.prototype.trim() :-
+// removes whitespace from both ends of a string and returns a new string, without modifying the original string.
+
+const greeting = '   Hello world!   ';
+console.log(greeting.trim());
+
+let newStr = greeting.trim();
+console.log(newStr)
+
+// ---------------------------------------------------------------------------------------------------------
