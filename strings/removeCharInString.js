4afc6fb77d170642c4b333e0e69a1f20850758b3
Raviegupta <raviegupta153@gmail.com>
2023-03-13 19:58:14 +0530

    String.prototype.replaceAll()

--- strings/removeCharInString.js
+++ strings/removeCharInString.js
@@ -10,7 +10,29 @@ const removeChar = (str, char) => {
   }
   return newStr;
 };
-console.log(removeChar("ChandigArh", "a"));
+console.log(removeChar("ChandigArhaaaAaAa", "a"));
 
 
+// ---------------------------------------------------------------------------------------------------------
+// using String.prototype.replaceAll() :-
+// The replaceAll() method returns a new string with all matches of a pattern replaced by a replacement.
+// The pattern can be a string or a RegExp,
+// replacement can be a string or a function to be called for each match
+
+// syntax:- replaceAll(pattern, replacement)
+
+
+let str = "ChandigArhaaaAaAa" 
+let str1 = (str.replaceAll('a', ''));
+let str2 = (str1.replaceAll('A', ''));
+console.log(str2);
+
+
+removeChar2 = (str, char) => {
+  let str1 = str.replaceAll(char.toUpperCase(), '');
+  let str2 = str1.replaceAll(char.toLowerCase(), ''); 
+  return str2;
+};
+console.log(removeChar2("ChandigArhaaaAaAa", "a"));
+console.log(removeChar2("PiPnk panPtherp", "P"));
 
