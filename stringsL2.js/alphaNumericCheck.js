ab26d71767e9b838a9c7784de12d8b1a31222751
Raviegupta <raviegupta153@gmail.com>
2023-03-14 21:19:45 +0530

    alphanumeric check

--- stringsL2.js/alphaNumericCheck.js
+++ stringsL2.js/alphaNumericCheck.js
@@ -1,3 +1,29 @@
 // alphanumeric :- 
 // ravie :- No
 // raviegupta153 :- Yes
+
+
+// console.log('a'%1 == 0);
+// console.log('@'%1 == 0);
+// console.log('0'%1 == 0);
+// console.log('3'%1 == 0);
+
+// ******************************************
+// console.log(( (" "% 1) == 0))  
+// ******************************************
+
+
+const isAlphaNumeric = (str) => {
+    let check = false;
+    for(let i=0; i<str.length; i++) {
+        if(str[i]%1 == 0 && str[i] != " ") {
+            check = true;
+        }
+    }
+    return check == true;
+}
+console.log(isAlphaNumeric('alphanumeric'));
+console.log(isAlphaNumeric('raviegupta153@gmail.com'));
+console.log(isAlphaNumeric('Hello World'));
+
+// ---------------------------------------------------------------------------------------------------------
