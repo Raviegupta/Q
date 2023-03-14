b2bd10d0bec3372080e04506af4beac9152cf639
Raviegupta <raviegupta153@gmail.com>
2023-03-14 14:06:24 +0530

    Exactly reversing the string

---
+++ strings/stringReversing.js
@@ -0,0 +1,18 @@
+// Given a string, return the string in reverse
+// https://replit.com/@ac102-batch/String-Reverse-Raviegupta
+
+const reverse = (str) => {
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
+};
+console.log(reverse("javascript is beautiful"));
+
+const reverse2 = (str) => {
+  let arrChars = str.split("");
+  arrChars.reverse();
+  return arrChars.join("");
+};
+console.log(reverse2("javascript is awsome"));
