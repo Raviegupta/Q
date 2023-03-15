3750195ad84ad37515395df8ce3b90fc428a9ff7
Raviegupta <raviegupta153@gmail.com>
2023-03-15 22:48:20 +0530

    update

--- puzzles/playGameWithNoEyeStrain.js
+++ puzzles/playGameWithNoEyeStrain.js
@@ -8,16 +8,16 @@
 // https://replit.com/@ac102-batch/Eye-Strain-Raviegupta#index.js
 
 const game = (x, y, z) => {
-    let playTime = breakTime = totalTime = 0;
-    playTime = x*y;
-    x>3 && x%3!=0 ? breakTime = (Math.floor(x/3))*z : "khelte raho"
-    x%3==0 ? breakTime = (Math.floor(x/3)-1)*z : "enjoy"
-    totalTime = playTime + breakTime; 
-    return totalTime;
-  }
-  console.log(game(2, 12, 10))  //24
-  console.log(game(3, 12, 10))  //36
-  console.log(game(4, 12, 10))  //58
-  console.log(game(5, 12, 10))  //70
-  console.log(game(6, 12, 10))  //82
-  console.log(game(7, 12, 10))  //104
+  let playTime = (breakTime = totalTime = 0);
+  playTime = x * y;
+  x > 3 && x % 3 != 0 ? (breakTime = Math.floor(x / 3) * z) : "khelte raho";
+  x % 3 == 0 ? (breakTime = (Math.floor(x / 3) - 1) * z) : "enjoy";
+  totalTime = playTime + breakTime;
+  return totalTime;
+};
+console.log(game(2, 12, 10)); //24
+console.log(game(3, 12, 10)); //36
+console.log(game(4, 12, 10)); //58
+console.log(game(5, 12, 10)); //70
+console.log(game(6, 12, 10)); //82
+console.log(game(7, 12, 10)); //104
