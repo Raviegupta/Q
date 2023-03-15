2900c466fea337530023ee2781b316ab8721402b
Raviegupta <raviegupta153@gmail.com>
2023-03-15 23:58:23 +0530

    first10multiple

--- maths/first10multiple.js
+++ maths/first10multiple.js
@@ -11,3 +11,17 @@ const table = (num) => {
   return str;
 };
 console.log(table(7))
+
+
+function table2(n) {
+  let str = '';
+  for (let i = 1; i <= 10; i++) {
+    if (i == 10) {
+      str += (n * i) + 1;
+    } else {
+      str += (n * i) + 1 + ',';
+    }
+  }
+  return str;
+}
+console.log(table2(7));
