95b365fbcc1fe29aeaa29ace9cd12a3e4f7b93c7
Raviegupta <raviegupta153@gmail.com>
2023-03-17 11:48:44 +0530

    lcm

--- maths/hcf-lcm.js
+++ maths/hcf-lcm.js
@@ -7,9 +7,29 @@ const hcf = (a, b) => {
   }
   return hcf;
 };
-console.log(hcf(4, 8));
-console.log(hcf(2, 7));
-console.log(hcf(32, 64));
-
+// console.log(hcf(4, 8));
+// console.log(hcf(2, 7));
+// console.log(hcf(32, 64));
 
 // ---------------------------------------------------------------------------------------------------------
+// lcm
+
+const lcm = (a, b) => {
+  let l = 0,
+    s = Math.min(a, b),
+    lcm,
+    check = false;
+  for (let i = 1; i >= 1; i++) {
+    l += Math.max(a, b);
+    if (l % s == 0) {
+      lcm = l;
+      check = true;
+    }
+    if (check == true) break;
+  }
+  return lcm;
+};
+console.log(lcm(5, 16));
+console.log(lcm(21, 5));
+console.log(lcm(2, 47));
+console.log(lcm(5, 25));
