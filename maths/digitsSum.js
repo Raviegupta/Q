fea9250844ad43e6c70f38a6fd33635010ec9249
Raviegupta <raviegupta153@gmail.com>
2023-03-17 12:32:55 +0530

    even digits in the number

--- maths/digitsSum.js
+++ maths/digitsSum.js
@@ -10,3 +10,22 @@ const sum = (num) => {
     return sum;
 }
 console.log(sum(9876));
+
+
+
+// ---------------------------------------------------------------------------------------------------------
+// display the even digits
+const evenDigits = (num) => {
+    let check = false, rem;
+    while(num > 0) {
+        rem = num%10;
+        if(rem%2==0) {
+            console.log(rem);
+            check = true;
+        }
+        num = Math.floor(num/10);
+    }
+    return check==false ? 'No even digits' : '✅';
+}
+console.log(evenDigits(9876));
+console.log(evenDigits(357));
