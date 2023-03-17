80fa17b23b2c6ae56b7755d2f9d3ea5244779508
Raviegupta <raviegupta153@gmail.com>
2023-03-17 20:50:05 +0530

    series2

--- maths/series.js
+++ maths/series.js
@@ -3,10 +3,36 @@
 
 
 const series = (N) => {
-    let sum =0;
+    let sum = 0;
     for(let i=1; i<=N; i++) {
         sum += i*2
     }
     return sum;
 }
 console.log(series(6));
+
+
+
+// ---------------------------------------------------------------------------------------------------------
+// S = 1!+2!+3!+4!....N!  (upto N terms)
+
+const fact = (num) => {
+    let fact = 1;
+    for(let i=1; i<=num; i++) {
+        fact *= i; 
+    }
+    return fact;
+}
+
+const series2 = (N) => {
+    let sum = 0;
+    for(let i=1; i<=N; i++) {
+        sum += fact(i);
+    }
+    return sum;   
+}
+console.log(series2(4))
+console.log(series2(5))
+
+
+// ---------------------------------------------------------------------------------------------------------
