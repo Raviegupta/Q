0bf7649030c9b2a1996b929493762af85e6edb41
Raviegupta <raviegupta153@gmail.com>
2023-03-17 22:58:11 +0530

    noIntoString without string method

---
+++ stringsL2.js/noIntoString.js
@@ -0,0 +1,12 @@
+// Convert a no into string without using string method and return it's typeof.
+
+const noIntoString = (num) => {
+    let str = num + '';
+    // console.log(num, str);
+    return typeof(str);
+}
+console.log(noIntoString(75));
+
+
+
+// ---------------------------------------------------------------------------------------------------------
