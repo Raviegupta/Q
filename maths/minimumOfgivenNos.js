b4f8e7d77a94bca01bda14f7255a90eaad17e558
Raviegupta <raviegupta153@gmail.com>
2023-03-17 22:49:57 +0530

    Math.min()

--- maths/minimumOfgivenNos.js
+++ maths/minimumOfgivenNos.js
@@ -1,4 +1,5 @@
 // find the minimum of the given three nos without using Math functions.
+// or implement Math.min()
 
 const minimum = (a, b, c) => {
     if(a<b && a<c) return a;
@@ -8,3 +9,13 @@ const minimum = (a, b, c) => {
 console.log(minimum(5,7,3));
 console.log(minimum(4,2,9));
 
+
+const minimum2 = (a, b, c) => {
+    let min = Math.min(a, b, c);
+    return min;
+}
+console.log(minimum(5, 7, 3));
+
+
+
+// ---------------------------------------------------------------------------------------------------------
