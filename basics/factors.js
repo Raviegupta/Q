7cc036e45a5bec55da8bdd5ffbe4eb32983fb565
Raviegupta <raviegupta153@gmail.com>
2023-03-17 23:18:58 +0530

    sum of factors

--- basics/factors.js
+++ basics/factors.js
@@ -22,8 +22,8 @@ const factorsSum = (num) => {
   let sum = 0;
   for(let i=1; i<=num; i++){
     if(num%i == 0) {
-      console.log(i);
-      // sum += i;
+      // console.log(i);
+      sum += i;
     }
   }
   return sum;
