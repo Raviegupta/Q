370d1b9f283c63222f8655bdebb4e5566f0803ca
Raviegupta <raviegupta153@gmail.com>
2023-03-20 23:22:20 +0530

    string manipulation

--- strings/stringReversing.js
+++ strings/stringReversing.js
@@ -17,42 +17,36 @@ const reverse2 = (str) => {
 };
 // console.log(reverse2("javascript is awsome"));
 
-
-
 // ---------------------------------------------------------------------------------------------------------
 // reverse the string word by word
 // https://replit.com/@ac102-batch/String-Reverse-I-Raviegupta
 
 const reverseByWords = (str) => {
-  let arrWords = str.split(' ').reverse().join(' ')
-  return arrWords
-}
+  let arrWords = str.split(" ").reverse().join(" ");
+  return arrWords;
+};
 // console.log(reverseByWords('the sky is blue'));
 
-
 const reverseByWords2 = (str) => {
-  let arrWords = str.split(' ');
-  let newStr = '';
-  for(let i=(arrWords.length)-1; i>=1; i--) {
-    newStr += arrWords[i] + ' ';
+  let arrWords = str.split(" ");
+  let newStr = "";
+  for (let i = arrWords.length - 1; i >= 1; i--) {
+    newStr += arrWords[i] + " ";
   }
   newStr += arrWords[0];
   return newStr;
-}
+};
 // console.log(reverseByWords2('the sky is blue'));
 
-
 const reverseByWords3 = (str) => {
-  let arrWords = str.split(' ');
-  let newStr = '';
-  for(let i=(arrWords.length)-1; i>=0; i--) {
-    i>0 ? newStr += arrWords[i] + ' ' : newStr += arrWords[0]
+  let arrWords = str.split(" ");
+  let newStr = "";
+  for (let i = arrWords.length - 1; i >= 0; i--) {
+    i > 0 ? (newStr += arrWords[i] + " ") : (newStr += arrWords[0]);
   }
   return newStr;
-}
+};
 // console.log(reverseByWords3('the sky is blue'));
-
-
 
 // ---------------------------------------------------------------------------------------------------------
 // string manipulation1
@@ -60,51 +54,42 @@ const reverseByWords3 = (str) => {
 // Ravie Gupta  =>  eiveR atpuG
 
 const reverse3 = (str) => {
-  let arrWords = str.split(' ');
-  let newStr = '';
-  for(let i=0; i<arrWords.length; i++) {
-    for(let j= arrWords[i].length-1; j>=0;j--) {
+  let arrWords = str.split(" ");
+  let newStr = "";
+  for (let i = 0; i < arrWords.length; i++) {
+    for (let j = arrWords[i].length - 1; j >= 0; j--) {
       newStr += arrWords[i][j];
     }
-    newStr += " "; 
-  }  
+    newStr += " ";
+  }
   // console.log(newStr);
-}
+};
 // console.log(reverse3("Ravie Gupta"));
 // console.log(reverse3("Chandigarh The City Beautiful"));
-
-
 
 // ---------------------------------------------------------------------------------------------------------
 // string manipulation2
 // city chandigarh  =>  ccihtayndigarh
 
-const reverse4 = (str) => {
-  let arrWords = str.split(' ');
-  console.log(arrWords);
-  let newStr = '', j =0;
-  for(let i=0; i<arrWords.length; i++) {
-    while(arrWords[i][j] == undefined) {
-
-    }
-  }
-
-}
-console.log(reverse4('The city chandigarh'))
-
-
 const reverse5 = (str) => {
-  let arrWords = str.split(' ');
-  console.log(arrWords);
-  let newStr = '', j =0;
-  for(k=0; k<=4; k++) {
-    for(let i=0; i<arrWords.length; i++) {
-    newStr += arrWords[i][j];
+  let arrWords = str.split(" ");
+  let mxLength = 0,
+    newStr = '';
+  for (let i = 0; i < arrWords.length; i++) {
+    if (mxLength < arrWords[i].length) {
+      mxLength = arrWords[i].length;
     }
-    newStr += ' '
-    j++;
   }
-    console.log(newStr);
+  for (let i = 0; i < mxLength; i++) {
+    for (let j = 0; j < arrWords.length; j++) {
+      if (arrWords[j].length > i) {
+        newStr += arrWords[j][i];
+      }
+    }
+    newStr += " ";
   }
-  
-console.log(reverse5('ravie gupta'));
+  return (newStr);
+};
+
+console.log(reverse5("The city chandigarh"));
+console.log(reverse5("I Love My India"));
