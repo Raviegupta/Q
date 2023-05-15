5566e0c8bae86bc488af702a4e524a57f860bb99
Raviegupta <raviegupta153@gmail.com>
2023-05-15 05:36:07 +0530

    memoization

--- ac-ds/linked-list.js
+++ ac-ds/linked-list.js
@@ -12,6 +12,7 @@
 //                                          => 2) store reference of next data(node) or null  => we say next
 
 
+
 //        N1                                 N2                          N3
 //    ------------                      ------------                ------------
 //   | 2  | next  |                    | 4  | next  |              | 6  | next  |
@@ -25,7 +26,13 @@
 // N2.next.data = 6;
 
 
+// LL is Better in terms of
+// Time Complexity
+// Space Complexity
+
 // ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+
 function newLLNode(value) {
     this.data = value;
     this.next = null;
@@ -41,7 +48,9 @@ function newLLNode(value) {
 // console.log(n2.data);
 // console.log(n3.data);
 
+
 // using new keyword to avoid above issues.
+// ---------------------------------------
 // const n1 = new newLLNode(2);
 // console.log(n1.data);
 // const n2 = new newLLNode(4);
@@ -53,14 +62,76 @@ function newLLNode(value) {
 // console.log(n3.data);
 
 // ----------------------------------------------------------------------------------------
-const n1 = new newLLNode(2);
-const n2 = new newLLNode(4);
-n1.next = n2;
-console.log(n1.data);
-console.log(n1.next);
-console.log(n1.next.data);
-console.log(n1);
+// ex:-
+// const n1 = new newLLNode(2);
+// const n2 = new newLLNode(4);
+// n1.next = n2;
+// console.log(n1.data);
+// console.log(n1.next);
+// console.log(n1.next.data);
+// console.log(n1);
+
+// ----------------------------------------------------------------------------------------
+// Head :- keep the reference of node1
+
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+// bulid a linked list
+// 1, 3, 5, 7, 9
+
+// head of a linked list is a reference of a first node in LL
+let head = null;
+
+function insertIntoLL(node) {
+    if(head == null) {
+        head = node;
+    } else {
+        let temp = head;
+        while(temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = node;
+    }
+}
+
+function getSizeOfLL(head) {
+
+    let size = 0;
+    let temp = head;
+
+    while(temp != null) {
+        size++;
+        temp = temp.next;
+    }
+
+    return size;
+
+}
+
+function deleteNode(head, value) {     // delete a node having data = given value // Assume LL has unique values.
 
 
+
+
+}
+
+
+
+for(let i = 1; i <= 5; i++) {
+    let value = 2*i -1                  // creating a random value
+    let node = new newLLNode(value);    // creating a value
+
+    insertIntoLL(node);           // a function that helps us to add node in LL.
+}
+
+
+
+// console.log(head);
+
+
+
+
+
+// -----------------------------------------------------------------------------------
 
 
--- basics/007-fibonacci.js
+++ basics/007-fibonacci.js
@@ -1,6 +1,7 @@
 // Fibonacci Series :- The Fibonacci sequence is a set of integers (the Fibonacci numbers) that starts with a zero, followed by a one, then by another one, and then by a series of steadily increasing numbers. The sequence follows the rule that each number is equal to the sum of the preceding two numbers.
 // https://replit.com/@ac102-batch/Number-of-Student-Raviegupta
 
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181.
 const fibonacci = (a, b, n) => {
   console.log(a);
   console.log(b);
@@ -14,3 +15,26 @@ const fibonacci = (a, b, n) => {
 };
 console.log(fibonacci(0, 1, 10));
 console.log(fibonacci(20, 30, 10));
+
+
+// solving using recursion
+function fibo(n) {
+  if(n<2) return n;
+  else return fibo(n-2) + fibo(n-1);
+}
+console.log(fibo(2));
+console.log(fibo(8));
+
+
+// now solving using the memoization
+const fib = (n, memo) => {
+  memo = memo || {}
+
+  if (memo[n]) return memo[n]
+
+  if (n < 2) return n
+  return memo[n] = fib(n-2, memo) + fib(n-1, memo)
+}
+
+console.log(fib(2));
+console.log(fib(8));
